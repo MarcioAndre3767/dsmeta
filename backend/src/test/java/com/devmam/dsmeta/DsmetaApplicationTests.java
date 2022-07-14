323fec41eb3ffefa1068cf694dedbe4212f02885
Marcio Andre Mendes <marcioandre.ti1@gmail.com>
2022-07-14 01:25:17 -0300

    Project created

---
+++ backend/src/test/java/com/devmam/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmam.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
